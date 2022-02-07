61c70cce881370fba66bf0a84334ca6e5f52e909
Robert Paton <bobbypaton@users.noreply.github.com>
2022-02-07 10:53:00 -0700

    Update test_pyqrc.py

--- tests/test_pyqrc.py
+++ tests/test_pyqrc.py
@@ -8,7 +8,7 @@ from pyqrc import pyQRC as QRC
 from .conftest import datapath
 
 @pytest.mark.parametrize("path, nproc, mem", [
-    ('acetaldehyde.log', 4,  '8GB'),
+    ('Gaussian/acetaldehyde.log', 4,  '8GB'),
 ])
 def test_QRC(path, nproc, mem):
     path = datapath(path)
